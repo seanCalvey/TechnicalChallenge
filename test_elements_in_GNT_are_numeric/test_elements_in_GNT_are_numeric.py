ebe4d085d82e35453756f901f3b8bee002960cfa
calsean86@outlook.ie <Zqn8T9$8>
2021-09-27 17:30:54 +0100

    payslip

---
+++ test_elements_in_GNT_are_numeric/test_elements_in_GNT_are_numeric.py
@@ -0,0 +1,26 @@
+import unittest
+import pandas as pd
+
+
+def check_columns_type(series):
+    for cell_type in series.values:
+        if cell_type == int or float:
+            print("int")
+        else:
+            return False
+    return True
+
+
+class MyTestCase(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.GNT_file = pd.read_excel("data_pass/GTN.xlsx")
+
+    def test_data_cell_in_GNT_are_numeric(self):
+        pay_elements = self.GNT_file.dtypes[4:]
+        self.assertTrue(check_columns_type(pay_elements))
+
+
+if __name__ == '__main__':
+    unittest.main()
